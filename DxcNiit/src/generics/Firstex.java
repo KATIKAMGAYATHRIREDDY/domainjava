a9bf8a5636bcbeaf6c0fc0dc8ddc485914726471
katik <katik@DESKTOP-GHI634P>
2021-04-06 12:18:58 +0530

    Collection,File IO,Generics

--- DxcNiit/src/generics/Firstex.java
+++ DxcNiit/src/generics/Firstex.java
@@ -7,11 +7,12 @@ public static void main(String[] args) {
 	String[] stu = new String[3];
 	stu[0]="gayathri";
 	//stu[1]=10;
-	ArrayList stli = new ArrayList();
+	String name= stu[0];//no need to typecast
+	ArrayList<String> stli = new ArrayList<String>();
 	stli.add("gayathri");
-	stli.add(new Integer(10));
+	//stli.add(new Integer(10));
 	System.out.println(stli);
-	String name = (String) stli.get(1);
-	System.out.println(name);
+	String name1 = (String) stli.get(1);//need to typecast
+	System.out.println(name1);
 }
 }
